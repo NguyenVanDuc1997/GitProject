cc3cd473e6f9d191be3bdcae5be75835cbe536e7
NguyenVanDuc1997 <nguyenvanduc160797@gmail.com>
2020-03-05 17:40:25 +0700

    bổ sung tam giác

--- BT_Ly_Thuyet/Tam_Giac/BT_Tam_Giac.js
+++ BT_Ly_Thuyet/Tam_Giac/BT_Tam_Giac.js
@@ -1,34 +1,29 @@
 function tinh() {
-    let a=document.getElementById('canh_a').value;
+    let a=document.getElementById('edge_a').value;
     a=parseFloat(a);
-    let b=document.getElementById('canh_b').value;
+    let b=document.getElementById('edge_b').value;
     b=parseFloat(b);
-    let c=document.getElementById('canh_c').value;
+    let c=document.getElementById('edge_c').value;
     c=parseFloat(c);
     if (a+b>c&&a+c>b&&b+c>a){
-        document.getElementById('tamgiac').value='Đây là tam giác';
-        let cv=a+b+c;
-        let dt=Math.sqrt((a*a+b*b+c*c)**2-2*(a**4+b**4+c**4))/4;
-        document.getElementById('chuvi').value=cv;
-        document.getElementById('dientich').value=dt;
+        document.getElementById('triangle').value='Đây là tam giác';
+        let premeter=a+b+c;
+        let area=Math.sqrt((a*a+b*b+c*c)**2-2*(a**4+b**4+c**4))/4;
+        document.getElementById('premeter').value=premeter;
+        document.getElementById('area').value=area;
 
         if (a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a){
-            document.getElementById('loai').value='vuông'
+            document.getElementById('typee').value='vuông'
             if (a==b||a==b||b==c){
-                document.getElementById('loai').value='vuông cân';
+                document.getElementById('typee').value='vuông cân';
             }
         }else if (a==b&&a==c&&b==c){
-            document.getElementById('loai').value='đều';
-        }else if (a==b||a==b||b==c){
-            document.getElementById('loai').value='cân'
+            document.getElementById('typee').value='đều';
+        }else if (a==b||a==c||b==c){
+            document.getElementById('typee').value='cân'
         }else
-            document.getElementById('loai').value='Thường'
+            document.getElementById('typee').value='Thường'
     }else {
         document.getElementById('tamgiac').value='Đây không phải tam giác'
     }
-}
-function can() {
-    let a=document.getElementById('can').value;
-    z=Math.sqrt(a);
-    document.getElementById('canh_c').value=z;
 }
